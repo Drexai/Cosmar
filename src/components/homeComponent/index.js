c6da0f27f4ab6de2760c2e4ae54b9c788aafb76d
Maximiliano Scollo <Maximilianoscollo17@gmail.com>
2024-02-20 18:02:53 -0300

    First commit of the proyecy

---
+++ src/components/homeComponent/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+
+
+export const HomeComponent = () => {
+
+return(
+    <>
+        <header>
+            <nav>
+                <imput type="search">
+
+                </imput>
+            </nav>
+        </header>
+        <body>
+            <main>
+                <section>
+
+                </section>
+            </main>
+        </body>
+    </>
+)
+}
