4e7b0da3ca30490e1e6b527e152dbbfa73770f1d
Maximiliano Scollo <Maximilianoscollo17@gmail.com>
2024-02-20 18:46:48 -0300

    Tailwind instalation and minor fixes

---
+++ src/components/userComponent/index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+
+
+
+export const UserComponent = () => {
+
+return(
+    <>
+        <header>
+            <nav>
+                <imput type="search">
+
+                </imput>
+            </nav>
+        </header>
+        <body>
+            <main>
+                <section>
+
+                </section>
+            </main>
+        </body>
+    </>
+)
+}
