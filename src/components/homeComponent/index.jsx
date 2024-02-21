6599761468f5ec944fda9eff0c599acec5ac8a5d
Maximiliano Scollo <Maximilianoscollo17@gmail.com>
2024-02-21 19:33:36 -0300

    Home component progress

--- src/components/homeComponent/index.jsx
+++ src/components/homeComponent/index.jsx
@@ -1,26 +1,52 @@
 import React from "react";
 
 
-
-
 export const HomeComponent = () => {
 
-return(
-    <>
-        <header>
+return (
+    <> 
+        <header class="w-full h-5" >
             <nav>
-                <imput type="search">
-
-                </imput>
-            </nav>
+                <div class="relative w-full max-w-xl mx-auto bg-white rounded-full h-full lg:max-w-none">
+                    <imput type="search" placeholder="Busqueda de productos" class="rounded-full w-full h-18 bg-transparent py-0 pl-8 pr-32 outline-none border-2 border-gray-100 shadow-md hover:outline-none focus:ring-cool-indigo-200 focus:border-cool-indigo-200" />
+                </div>    
+            </nav>    
         </header>
-        <body>
-            <main>
-                <section>
-
-                </section>
-            </main>
-        </body>
+        <main>
+            <section class="max-w-lg px-4 pt-12 mx-auto md:max-w-screen-2xl md:px-6 xl:px-8 2xl:px-12">
+                <div classname="flex flex-col w-full overflow-hidden bg-gray-100 rounded-2xl h-72 sm:h-80 md:h-72 lg:h-64 xl:h-80">
+                    <h2 class="flex items-center text-xl font-bold leading-7 text-gray-900 group-hover:text-cool-indigo-600">
+                        Facturación
+                    </h2>
+                </div>
+                <div class="flex flex-col w-full overflow-hidden bg-gray-100 rounded-2xl h-72 sm:h-80 md:h-72 lg:h-64 xl:h-80">
+                    <h2 class="flex items-center text-xl font-bold leading-7 text-gray-900 group-hover:text-cool-indigo-600">
+                        Stock
+                    </h2>
+                </div>
+                <div class="flex flex-col w-full overflow-hidden bg-gray-100 rounded-2xl h-72 sm:h-80 md:h-72 lg:h-64 xl:h-80">
+                    <h2 class="flex items-center text-xl font-bold leading-7 text-gray-900 group-hover:text-cool-indigo-600">
+                        Clientes
+                    </h2>
+                </div>
+                <div class="flex flex-col w-full overflow-hidden bg-gray-100 rounded-2xl h-72 sm:h-80 md:h-72 lg:h-64 xl:h-80">
+                    <h2 class="flex items-center text-xl font-bold leading-7 text-gray-900 group-hover:text-cool-indigo-600">
+                        Remito
+                    </h2>
+                </div>
+                <div class="flex flex-col w-full overflow-hidden bg-gray-100 rounded-2xl h-72 sm:h-80 md:h-72 lg:h-64 xl:h-80">
+                    <h2 class="flex items-center text-xl font-bold leading-7 text-gray-900 group-hover:text-cool-indigo-600">
+                        Usuarios
+                    </h2>
+                </div>
+                <div class="flex flex-col w-full overflow-hidden bg-gray-100 rounded-2xl h-72 sm:h-80 md:h-72 lg:h-64 xl:h-80">
+                    <h2 class="flex items-center text-xl font-bold leading-7 text-gray-900 group-hover:text-cool-indigo-600">
+                        Presupuestos
+                    </h2>
+                </div>
+            </section>
+        </main>            
+                   
     </>
 )
 }
