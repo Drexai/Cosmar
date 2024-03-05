aeb0c9bb93242758a0876c008312100b53619bde
Maximiliano Scollo <Maximilianoscollo17@gmail.com>
2024-03-05 10:51:01 -0300

    minor responsive fix

--- src/components/homeComponent/index.jsx
+++ src/components/homeComponent/index.jsx
@@ -1,7 +1,4 @@
 import React from "react";
-import { ReactSVG } from "react";
-
-
 
 export const HomeComponent = () => {
 
@@ -11,17 +8,18 @@ return (
             <nav class="relative w-full max-w-xl mx-auto bg-white rounded-full h-10 lg:max-w-90">
                 <div class="relative w-full h-10 max-w-xl mx-auto bg-white rounded-full lg:max-w-none">
                     <imput type="search" placeholder="Busqueda de productos" class="rounded-full w-full h-10  bg-transparent  outline-none border-2 border-gray-100 shadow-md hover:outline-none focus:ring-cool-indigo-200 focus:border-cool-indigo-200" >
-                        <button type="" class="absolute inline-flex items-center h-10  text-sm text-black transition duration-150 duration-300 ease-in-out rounded-full outline-none right-2 top-1 bg-cool-indigo-600 sm:px-6 sm:text-base sm:font-medium hover:bg-cool-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cool-indigo-500">
+                
+                    </imput>
+                    <button type="" class="absolute inline-flex items-center h-10  text-sm text-black transition duration-150 duration-300 ease-in-out rounded-full outline-none right-1 top-0 bg-cool-indigo-600 sm:px-6 sm:text-base sm:font-medium hover:bg-cool-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cool-indigo-500">
                             <svg class="-ml-0.5 sm:-ml-1 mr-2 w-4 h-4 sm:h-5 sm:w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
                             </svg>
-                        </button>
-                    </imput>
+                    </button>
                 </div>    
             </nav>    
         </header>
-        <main class="flex justify-around  w-full min-[768px]:h-svh  bg-orange-200">
-            <section class=" h-full grid mx-auto gap-x-6 max-sm:gap-y-12  min-[425]:grid-cols-2 md:grid-cols-3 xl:gap-x-8 ">
+        <main class="flex justify-around  w-full min-h-screen  bg-orange-200">
+            <section class=" h-full grid mx-auto gap-x-6 max-sm:gap-y-12  min-[425]:grid-cols-2 md:grid-cols-3 md:gap-y-8 xl:gap-x-8 ">
                 <div class="flex flex-col overflow-hidden bg-gray-100 rounded-2xl h-48 w-48">
                     <h2 class="flex items-center text-xl font-bold leading-7 text-gray-900 group-hover:text-cool-indigo-600">
                         Facturación
@@ -31,7 +29,7 @@ return (
                     <h2 class="flex items-center text-xl font-bold leading-7 text-gray-900 group-hover:text-cool-indigo-600">
                         Stock
                     </h2>
-                    <svg src="C:\Users\Maxim\OneDrive\Escritorio\react\cosmar\src\assets\stock-svgrepo-com.svg"></svg>
+                    
                 </div>
                 <div class="flex flex-col overflow-hidden bg-gray-100 rounded-2xl h-48 w-48">
                     <h2 class="flex items-center text-xl font-bold leading-7 text-gray-900 group-hover:text-cool-indigo-600">
