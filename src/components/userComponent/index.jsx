a26a07aa7659a8d906ff54d4286b1839a38f35ce
Maximiliano Scollo <Maximilianoscollo17@gmail.com>
2024-03-09 00:00:46 -0300

    Form

--- src/components/userComponent/index.jsx
+++ src/components/userComponent/index.jsx
@@ -10,15 +10,67 @@ return(
     <>
         <header>
             <nav>
-                <imput type="search">
-
-                </imput>
+                <h1>Accesos</h1>
             </nav>
         </header>
         <body>
             <main>
-                <section>
+                <aside>
+                    <div>
+                        <h2>Crear nuevo usaurio</h2>
+                        <input type="text">
+                        </input>
+                    </div>
+                    <div>
+                        <h2>Contraseña</h2>
+                        <input type="password">
+                        </input>
+                    </div>
+                    
+                    <div>
+                        <h2>Repetir Contraseña</h2>
+                        <input type="password"> 
 
+                        </input>
+                    </div>
+                </aside>
+                <section>
+                    <div>
+                        <h2>Facturación</h2>
+                        <input type="checkbox"> 
+
+                        </input>
+                    </div>
+                    <div>
+                        <h2>Stock</h2>
+                        <input type="checkbox"> 
+
+                        </input>
+                    </div>
+                    <div>
+                        <h2>Otros</h2>
+                        <input type="checkbox"> 
+
+                        </input>
+                    </div>
+                    <div>
+                        <h2>Remito</h2>
+                        <input type="checkbox"> 
+
+                        </input>
+                    </div>
+                    <div>
+                        <h2>Clientes</h2>
+                        <input type="checkbox"> 
+
+                        </input>
+                    </div>
+                    <div>
+                        <h2>Otros</h2>
+                        <input type="checkbox"> 
+                            
+                        </input>
+                    </div>
                 </section>
             </main>
         </body>
