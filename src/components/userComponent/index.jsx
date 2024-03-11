f521240a9e771cc896e1547a34dab7504252bb3a
Maximiliano Scollo <Maximilianoscollo17@gmail.com>
2024-03-11 16:08:34 -0300

    Form

--- src/components/userComponent/index.jsx
+++ src/components/userComponent/index.jsx
@@ -15,63 +15,60 @@ return(
         </header>
         <body>
             <main>
-                <aside>
+                <fieldset>
+                    <legend>
+                        Gestion de usuario
+                    </legend>
                     <div>
-                        <h2>Crear nuevo usaurio</h2>
-                        <input type="text">
-                        </input>
+                        <h2></h2>
+                        < input type="text"/>
+                        <label htmlFor="dataOne"></label>
+                        
                     </div>
                     <div>
                         <h2>Contraseña</h2>
-                        <input type="password">
-                        </input>
+                        <input type="password"/>
+                        <label htmlFor="dataTwo"></label>
                     </div>
                     
                     <div>
                         <h2>Repetir Contraseña</h2>
-                        <input type="password"> 
-
-                        </input>
+                        <input type="password"/> 
+                        <label htmlFor="dataThree"></label>
                     </div>
-                </aside>
-                <section>
                     <div>
                         <h2>Facturación</h2>
-                        <input type="checkbox"> 
-
-                        </input>
+                        <input type="checkbox"/> 
+                        <label htmlFor="dataFour"></label>
+                        
                     </div>
                     <div>
                         <h2>Stock</h2>
-                        <input type="checkbox"> 
-
-                        </input>
+                        <input type="checkbox"/>
+                        <label htmlFor="dataFive"></label>
                     </div>
                     <div>
                         <h2>Otros</h2>
-                        <input type="checkbox"> 
-
-                        </input>
+                        <input type="checkbox"/> 
+                        <label htmlFor="dataSix"></label>
+                        
                     </div>
                     <div>
                         <h2>Remito</h2>
-                        <input type="checkbox"> 
-
-                        </input>
+                        <input type="checkbox"/>
+                        <label htmlFor="dataSeven"></label>
                     </div>
                     <div>
                         <h2>Clientes</h2>
-                        <input type="checkbox"> 
-
-                        </input>
+                        <input type="checkbox"/>
+                        <label htmlFor="dataEight"/> 
                     </div>
                     <div>
                         <h2>Otros</h2>
-                        <input type="checkbox"> 
-                            
-                        </input>
+                        <input type="checkbox"/>
+                        <label htmlFor="dataNine"></label> 
                     </div>
-                </section>
+                </fieldset>
             </main>
         </body>
     </>
