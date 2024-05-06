96397a401b99be63d71218b9035dac125ea03f6f
Maximiliano Scollo <Maximilianoscollo17@gmail.com>
2024-05-06 18:29:22 -0300

    react router install

---
+++ src/components/budgetComponent/index.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+export const BudgetComponent = () => {
+
+    return (
+        <>
+            <header>
+                <nav>
+                    <form action="">
+                        <ul>
+                            <li><label htmlFor=""></label>
+                                <input type="text" placeholder="Señores/señoras:" />
+                            </li>
+                            <li><label htmlFor="">
+                                </label><input placeholder="Cuenta N°:" type="text" />
+                            </li>
+                            <li><label htmlFor="">
+                                </label><input placeholder="N° Remito:" type="text" />
+                            </li>
+                            <li><label htmlFor="">
+                                </label><input placeholder="Domicilio" type="text" />
+                            </li>
+                            <li><label htmlFor="">
+                                </label><input input placeholder="Localidad" type="text" />
+                            </li>
+                            <li><label htmlFor="">
+                                </label><input laceholder="empleado" type="text" />
+                            </li>
+                        </ul>
+                    </form>
+                </nav>
+            </header>
+        </>
+    )
+}
+
