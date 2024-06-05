a8c673511531488ac015760da850d7bf946145d3
Maximiliano Scollo <Maximilianoscollo17@gmail.com>
2024-06-05 01:30:14 -0300

    Client IU first commit

--- src/components/clientsComponent/index.jsx
+++ src/components/clientsComponent/index.jsx
@@ -1,18 +1,20 @@
 import React from "react";
-
+import { Link } from 'react-router-dom';
 
 export const ClientComponent = () => {
 
     return (
         <>
-            <header class="relative py-10 w-full max-w-xl mx-auto bg-white flex flex-row">
-               <h2>filter / hacer filtro</h2>
-                <input placeholder="Escriba aquí para buscar...">
+            <header class="relative py-10 w-full  mx-auto bg-green-200 flex flex-row">
+                <Link to="/" class="h-20 w-20">
+                    <svg fill="#000000" viewBox="0 0 200 300" xmlns="http://www.w3.org/2000/svg"><title/><path d="M100,15a85,85,0,1,0,85,85A84.93,84.93,0,0,0,100,15Zm0,150a65,65,0,1,1,65-65A64.87,64.87,0,0,1,100,165ZM116.5,57.5a9.67,9.67,0,0,0-14,0L74,86a19.92,19.92,0,0,0,0,28.5L102.5,143a9.9,9.9,0,0,0,14-14l-28-29L117,71.5C120.5,68,120.5,61.5,116.5,57.5Z"/></svg>
+                </Link> 
+               <h2>Filter / hacer filtro</h2>
+                <input class="h-7 rounded-full text-center border-2 border-green-100  "  placeholder="Escriba aquí para buscar...">
                 </input>
             </header>
-            <main class="relative w-full max-w-xl mx-auto bg-white flex flex-row">
-                
-                <section class="flex flex-col w-2/4">
+            <main class="relative w-full h-screen  mx-auto  flex flex-row bg-green-200">
+                <section class="flex flex-col w-2/4 ">
                     <h2 class="w-full">
                         Lista de Clientes
                     </h2>   
@@ -35,12 +37,12 @@ export const ClientComponent = () => {
                 </section>
                 <aside class="flex flex-col w-2/4">
                     <form>
-                        <fieldset>
+                        <fieldset class="flex flex-col space-y-4 ">
                             <legend> Nuevo Cliente</legend>
-                            <input type="text" placeholder="Nombre"/>
-                            <input type="number" placeholder="Cuit"/>
-                            <input type="text" placeholder="Tipo contribuyente"/>
-                            <input type="text"placeholder="Alicuota" />
+                            <input class="max-[320px]: w-40" type="text" placeholder="Nombre"/>
+                            <input class="max-[320px]: w-40" type="number" placeholder="Cuit"/>
+                            <input class="max-[320px]: w-40" type="text" placeholder="Tipo contribuyente"/>
+                            <input class="max-[320px]: w-40" type="text"placeholder="Alicuota" />
                         </fieldset>
                         <button for="post"> Poner una imagen para enviar o sumar</button>
                     </form>
--- src/components/userComponent/index.jsx
+++ src/components/userComponent/index.jsx
@@ -9,10 +9,10 @@ return(
                 <Link to="/" class="h-20 w-20">
                     <svg fill="#000000" viewBox="0 0 200 300" xmlns="http://www.w3.org/2000/svg"><title/><path d="M100,15a85,85,0,1,0,85,85A84.93,84.93,0,0,0,100,15Zm0,150a65,65,0,1,1,65-65A64.87,64.87,0,0,1,100,165ZM116.5,57.5a9.67,9.67,0,0,0-14,0L74,86a19.92,19.92,0,0,0,0,28.5L102.5,143a9.9,9.9,0,0,0,14-14l-28-29L117,71.5C120.5,68,120.5,61.5,116.5,57.5Z"/></svg>
                 </Link>
-                <h1 class="flex justify-center">Accesos</h1>
+                <h1 class=" flex justify-center">Accesos</h1>
         </header>
         <main class="justify-center min-w-[364px] h-screen w-full  mx-auto bg-white flex flex-row bg-yellow-200">
-                <fieldset class=" h-3/4 w-3/4 bg-white flex flex-row bg-yellow-200 border-solid border-2 border-indigo-600">
+                <fieldset class=" h-3/4 w-3/4 2xl:w-2/4 bg-white flex flex-row bg-yellow-200 border-solid border-2 border-indigo-600">
                     <section class="flex flex-col border-solid border-2 border-indigo-600">
                         <h2 class="py-9 flex justify-center ">Gestion de usuario</h2>
                         <div>
