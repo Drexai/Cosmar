bbc8eec90131a9a000ce24cca7ce1f34ad697b72
Maximiliano Scollo <Maximilianoscollo17@gmail.com>
2024-06-26 02:26:56 -0300

    remit changes

--- src/components/remitComponent/index.jsx
+++ src/components/remitComponent/index.jsx
@@ -5,57 +5,57 @@ export const RemitComponent = () => {
 
     return (
         <>
-            <header class="relative w-full  mx-auto bg-green-200 flex flex-col">
-                <nav class="relative py-5  w-full h-full bg-green-200 flex justify-around">
+            <header class="relative w-full  mx-auto bg-green-200 flex flex-row">
                     <Link to="/" class="h-20 w-20">
                         <svg fill="#000000" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg"><title/><path d="M100,15a85,85,0,1,0,85,85A84.93,84.93,0,0,0,100,15Zm0,150a65,65,0,1,1,65-65A64.87,64.87,0,0,1,100,165ZM116.5,57.5a9.67,9.67,0,0,0-14,0L74,86a19.92,19.92,0,0,0,0,28.5L102.5,143a9.9,9.9,0,0,0,14-14l-28-29L117,71.5C120.5,68,120.5,61.5,116.5,57.5Z"/></svg>
                     </Link> 
-                    <aside class="flex flex-col">
-                        <button>
-                            <h2>Generar Factura A</h2>
-                        </button>
-                        <button>
-                            <h2>Generar Factura B</h2>
-                        </button>
-                        <button>
-                            <h2>Presupuesto</h2>
-                        </button>
-                        <button>
-                            <h2>Clientes Registrados</h2>
-                        </button>
-                    </aside>
-                    <form action="relative h-screen bg-orange-200 flex flex-row ">
-                        <ul>
-                            <li><label htmlFor=""></label>
-                                <input type="text" placeholder="Señores/señoras:" />
-                            </li>
-                            <li><label htmlFor="">
-                                </label><input placeholder="Cuenta N°:" type="text" />
-                            </li>
-                            <li><label htmlFor="">
-                                </label><input placeholder="N° Remito:" type="text" />
-                            </li>
-                            <li><label htmlFor="">
-                                </label><input placeholder="Domicilio" type="text" />
-                            </li>
-                            <li><label htmlFor="">
-                                </label><input input placeholder="Localidad" type="text" />
-                            </li>
-                            <li><label  placeholder="empleado" htmlFor="">
-                                    <input  type="text" />
+                <nav class="relative py-5  w-full h-full bg-green-200 flex justify-around">
+                    <form class="w-full flex flex-row flex justify-center">
+                            <div class="grid grid-cols-2  gap-7" >
+                                <label htmlFor="">
+                                    <input type="text" placeholder="Señores/señoras:" />
                                 </label>
-                            </li>
-                            <li><label placeholder="Iva" htmlFor="">
-                                    <input placeholder="Iva" type="checkbox"/>
+                                    
+                                <label htmlFor="">
+                                    <input placeholder="Cuenta N°:" type="text" />
                                 </label>
-                            </li>
-                            <li></li>
-                            <li></li>
-                        </ul>
+                                
+                                <label htmlFor="">
+                                    <input placeholder="Domicilio:" type="text" />
+                                </label>
+                                
+                                <label htmlFor="">
+                                    </label><input placeholder="Localidad" type="text" />
+                                
+                                <label htmlFor="">
+                                    </label><input input placeholder="Localidad" type="text" />
+                            </div>
+                            <div class="grid grid-cols-4 gap-4 place-content-center">
+                                <label  placeholder="Iva" htmlFor=""> IVA1 <input placeholder="Iva" type="checkbox"/></label>
+                                <label htmlFor=""> IVA 2 <input placeholder="Iva" type="checkbox"/> </label> 
+                                <label htmlFor=""> IVA 3 <input placeholder="Iva" type="checkbox"/> </label> 
+                                <label htmlFor=""> IVA 4 <input placeholder="Iva" type="checkbox"/> </label> 
+                                <label htmlFor=""> IVA 5 <input placeholder="Iva" type="checkbox"/> </label> 
+                                <label htmlFor=""> IVA 6 <input placeholder="Iva" type="checkbox"/> </label> 
+                                <label htmlFor=""> IVA 7 <input placeholder="Iva" type="checkbox"/> </label> 
+                                <label htmlFor=""> IVA 8 <input placeholder="Iva" type="checkbox"/> </label>     
+                            </div>
+                            <div>
+
+                                <label htmlFor="">
+                                    
+                                <input placeholder="Iva" type="checkbox"/>
+                                
+                                <input placeholder="Iva" type="checkbox"/>
+                                </label>
+                            </div>
+                        
                     </form>
                 </nav>
+                
+            </header>
                 <div class="relative w-2/4 h-10 max-w-xl mx-auto bg-white rounded-full lg:max-w-none">
-                        <input type="search" placeholder="Busqueda de productos"  class="rounded-full w-full h-10 bg-transparent  text-center outline-none border-2 border-gray-100 shadow-md hover:outline-none focus:ring-cool-indigo-200 focus:border-cool-indigo-200" >
+                        <input type="search" placeholder="Busqueda de productos"  class="rounded-full w-full lg:w-90 h-10 bg-transparent  text-center outline-none border-2 border-gray-100 shadow-md hover:outline-none focus:ring-cool-indigo-200 focus:border-cool-indigo-200" >
                     
                         </input>
                         <button type="" class="absolute inline-flex items-center h-8  
@@ -67,8 +67,7 @@ export const RemitComponent = () => {
                                 </svg>
                         </button>
                 </div>
-            </header>
-            <section class="relative  h-screen w-full     bg-orange-200  flex flex-row flex justify-center md:space-x-20">
+            <section class="relative  h-screen w-full     bg-green-200  flex flex-row flex justify-center md:space-x-20">
                 <div class="flex flex-col">
                     <h2>Código</h2>
                 </div>
