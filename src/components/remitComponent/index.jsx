8743d3c826b45f796dc49a190ef229f26ab0c079
Maximiliano Scollo <Maximilianoscollo17@gmail.com>
2024-06-28 00:43:28 -0300

    IU remit table fixes

--- src/components/remitComponent/index.jsx
+++ src/components/remitComponent/index.jsx
@@ -9,30 +9,30 @@ export const RemitComponent = () => {
                     <Link to="/" class="h-20 w-20">
                         <svg fill="#000000" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg"><title/><path d="M100,15a85,85,0,1,0,85,85A84.93,84.93,0,0,0,100,15Zm0,150a65,65,0,1,1,65-65A64.87,64.87,0,0,1,100,165ZM116.5,57.5a9.67,9.67,0,0,0-14,0L74,86a19.92,19.92,0,0,0,0,28.5L102.5,143a9.9,9.9,0,0,0,14-14l-28-29L117,71.5C120.5,68,120.5,61.5,116.5,57.5Z"/></svg>
                     </Link> 
-                <nav class="relative py-5  w-full h-full bg-green-300 flex justify-around max-md:flex-col">
-                    <form class="w-full flex flex-row flex justify-center">
-                            <div class="grid grid-cols-2 gap-7" >
-                                <label htmlFor="">
-                                    <input type="text" placeholder="Señores/señoras:" />
+                <nav class="relative py-5  w-full h-full bg-green-300 max-md:flex flex-col">
+                    <form class="w-full flex flex-row flex justify-evenly max-md:flex-col">
+                            <div class="grid grid-cols-2 gap-7 bg-zinc-500	py-2 rounded max-md:flex flex-col justify-center " >
+                                <label htmlFor="" class="max-md:flex justify-center">
+                                    <input class="rounded-full" type="text" placeholder="Señores/señoras:" />
                                 </label>
                                     
-                                <label htmlFor="">
-                                    <input placeholder="Cuenta N°:" type="text" />
+                                <label htmlFor="" class="max-md:flex justify-center">
+                                    <input class="rounded-full" placeholder="Cuenta N°:" type="text" />
                                 </label>
                                 
-                                <label htmlFor="">
-                                    <input placeholder="Domicilio:" type="text" />
+                                <label htmlFor="" class="max-md:flex justify-center">
+                                    <input class="rounded-full" placeholder="Domicilio:" type="text" />
                                 </label>
                                 
-                                <label htmlFor="">
-                                     <input placeholder="Localidad" type="text" />
+                                <label htmlFor="" class="max-md:flex justify-center">
+                                     <input class="rounded-full" placeholder="Localidad" type="text" />
                                 </label>
                                 
-                                <label htmlFor="">
-                                    <input input placeholder="Localidad" type="text" />
+                                <label htmlFor="" class="max-md:flex justify-center">
+                                    <input class="rounded-full" placeholder="Localidad" type="text" />
                                 </label>
                             </div>
-                            <div class="grid grid-cols-4 gap-4 place-content-center">
+                            <div class="grid grid-cols-4 gap-4 place-content-center bg-zinc-500	 rounded">
                                 <label  placeholder="Iva" htmlFor=""> IVA1 <input placeholder="Iva" type="checkbox"/></label>
                                 <label htmlFor=""> IVA 2 <input placeholder="Iva" type="checkbox"/> </label> 
                                 <label htmlFor=""> IVA 3 <input placeholder="Iva" type="checkbox"/> </label> 
@@ -42,21 +42,40 @@ export const RemitComponent = () => {
                                 <label htmlFor=""> IVA 7 <input placeholder="Iva" type="checkbox"/> </label> 
                                 <label htmlFor=""> IVA 8 <input placeholder="Iva" type="checkbox"/> </label>     
                             </div>
-                            <div>
-
-                                <label htmlFor="">
-                                    
-                                <input placeholder="Iva" type="checkbox"/>
-                                
-                                <input placeholder="Iva" type="checkbox"/>
+                            <div  class="flex flex-col justify-evenly bg-zinc-500	"  >
+                                    <h2>Condiciones de venta:</h2>
+                                <label htmlFor="" class="flex justify-end max-md:flex justify-center">
+                                    Contado
+                                    <input placeholder="Iva" type="checkbox"/>
+                                </label>
+                                <label htmlFor="" class="flex justify-end max-md:flex justify-center">
+                                    Cheque
+                                   <input placeholder="Iva" type="checkbox"/>
+                                </label>
+                                <label htmlFor="" class="flex justify-end max-md:flex justify-center">
+                                    Tarjeta de Credito
+                                    <input placeholder="Iva" type="checkbox"/>
                                 </label>
                             </div>
-                        
+                            <div  class="flex flex-col justify-evenly bg-zinc-500 rounded"  >
+                                <label htmlFor="" class="flex justify-end max-md:flex justify-center">
+                                    Reparto
+                                    <input placeholder="Iva" type="checkbox"/>
+                                </label>
+                                <label htmlFor="" class="flex justify-end max-md:flex justify-center">
+                                    A retirar Personalmente
+                                    <input placeholder="Iva" type="checkbox"/>
+                                </label>
+                            </div>
+                            <div class="border flex flex-col justify-around rounded bg-zinc-500	">
+                                <h2 class=" text black">Factura Nro</h2>
+                                <p class="text black">Insertar dato</p>
+                            </div>
                     </form>
                 </nav>
                 
             </header>
-
+            
             <main class="flex flex-col bg-green-300 ">
                 <div class="relative w-2/4 h-10 max-w-xl mx-auto bg-white rounded-full lg:max-w-none">
                     <input type="search" placeholder="Busqueda de productos"  class="rounded-full w-full lg:w-90 h-10 bg-transparent  text-center outline-none border-2 border-gray-100 shadow-md hover:outline-none focus:ring-cool-indigo-200 focus:border-cool-indigo-200" >
@@ -71,22 +90,18 @@ export const RemitComponent = () => {
                                 </svg>
                     </button>   
                 </div>
-                <section class="relative  h-screen w-full bg-green-300  flex flex-row flex justify-center md:space-x-20">
-                    <div class="flex flex-col">
-                        <h2>Código</h2>
-                    </div>
-                    <div class="flex flex-col">
-                        <h2>Cantidad</h2>
-                    </div>
-                    <div class="flex flex-col">
-                        <h2>Descripción</h2>
-                    </div>
-                    <div class="flex flex-col">
-                        <h2>Precio Unitario</h2>
-                    </div>
-                    <div class="flex flex-col">
-                        <h2>Precio Total</h2>
-                    </div>
+                <section class="h-screen w-full bg-green-300 md:space-x-20 ">
+                    <table class="h-full w-full ">
+                        <tr class="w-full flex justify-evenly ">
+                            <th>Código</th> 
+                            <th>Cantidad</th> 
+                            <th>Descripción</th>
+                            <th>Precio Unitario</th>
+                            <th>Precio Total</th> 
+                        </tr>
+                    </table>   
+                
+            
                 </section>
 
             </main>
